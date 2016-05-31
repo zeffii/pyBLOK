777e13b8273074b53afc21203e5a837c43c4cb1a
zeffii <tetha.z@gmail.com>
2016-05-31 09:52:24 +0200

    now can demo separate from core

--- core/blok_functions.py
+++ core/blok_functions.py
@@ -162,12 +162,12 @@ class Connect:
         return const.format(self.ID, self.FROM, self.TO, self.INPUTID)
 
 
-SubOsc1 = pBlk('Sub.Osc', (130, 140))
-SubOsc2 = pBlk('Sub.Osc', (130, 180))
-Env1 = pBlk('Env.advanced', (30, 180))
-con1 = Connect(Env1, SubOsc2, index=1)
-SubOsc1.params[2] = 0.333333
-Env1.set_params(attack=0.88, decay=0.45, amount=0.2)
+# SubOsc1 = pBlk('Sub.Osc', (130, 140))
+# SubOsc2 = pBlk('Sub.Osc', (130, 180))
+# Env1 = pBlk('Env.advanced', (30, 180))
+# con1 = Connect(Env1, SubOsc2, index=1)
+# SubOsc1.params[2] = 0.333333
+# Env1.set_params(attack=0.88, decay=0.45, amount=0.2)
 
 
-pCompile()
+# pCompile()
---
+++ core/pyBlok_demo.py
@@ -0,0 +1,11 @@
+from blok_functions import Connect, pBlk, pCompile
+
+SubOsc1 = pBlk('Sub.Osc', (130, 140))
+SubOsc2 = pBlk('Sub.Osc', (130, 180))
+Env1 = pBlk('Env.advanced', (30, 180))
+con1 = Connect(Env1, SubOsc2, index=1)
+SubOsc1.params[2] = 0.333333
+Env1.set_params(attack=0.88, decay=0.45, amount=0.2)
+
+
+pCompile()
